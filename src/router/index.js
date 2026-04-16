57892785f018e4b521df7f5fdb8f3182b31d99e3
dzdzFa <nikolaenkovdan@gmail.com>
2026-04-16 17:54:01 +0300

    Fix router for GitHub Pages and add base config

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { useAuth } from "../composables/useAuth";
 
 import HomeView from "../view/Home.vue";
@@ -78,7 +78,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, savedPosition) {
     if (to.hash) return { el: to.hash, behavior: "smooth" };
